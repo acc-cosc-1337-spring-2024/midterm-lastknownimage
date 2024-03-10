f0d005a2ecbd55572d6389aaf6dc1e91af95b3c8
Jovan <118417780+lastknownimage@users.noreply.github.com>
2024-03-10 01:35:02 +0000

    First part done

--- src/question_a/main.py
+++ src/question_a/main.py
@@ -1,1 +1,20 @@
-#add import
+def reversed_string(string1):
+    reversed_string = ''
+    index = len(string1) - 1
+    while index >= 0:
+        reversed_string += string1[index]
+        index -= 1
+    return reversed_string
+    
+def main():
+    while True:
+        user_input = input("Enter 'Hello world' or type 'exit' to leave.")
+        if user_input.lower() == 'exit':
+            print("Exiting.")
+            break
+        reversed_input = reversed_string(user_input)
+        print("Reversed:", reversed_input)
+
+if __name__ == "__main__":
+    main()
+        
--- src/question_a/question_a.py
+++ src/question_a/question_a.py
@@ -1,3 +1,30 @@
+def reverse_string(string1):
+    reverse_string = ' '
+    index = len(string1) - 1
+
+    while index >= 0:
+        reverse_string += string1[index]
+        index -= 1
+
+    return reverse_string
+
 #write functions here, don't add input('') statements here!
 def test_config():
     return True
+
+def test_reverse_string():
+    tests = [
+        ("Hello world", "dlrow olleH"),
+        ("Hello", "olleH"),
+    ]
+    for input_str, expected_ouput in tests:
+        result = reverse_string(input_str)
+        assert result == expected_ouput, "Didn't work for input '{}'. Looking for '{}', got '{} instead.".format(input_str, expected_ouput, result)
+        # The next part will look for the issue.
+        if result != expected_ouput:
+            print("That didn't work. Output was something unexpected.")
+            print(f"I wanted '{expected_ouput}', but I got '{result}'")
+        else:
+            print("Looks fine.")
+
+test_reverse_string()
